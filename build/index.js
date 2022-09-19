9d9167398ea7b739be25d98e44427a472e7f3dab
Carlos Eduardo Dos Santos <anjoazrael10@gmail.com>
2022-09-19 07:00:58 -0300

    create servidor api com express

---
+++ build/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.get("/", (req, resp, next) => {
+    return resp.json([
+        { id: 1, name: "Anuncio 1" },
+        { id: 2, name: "Anuncio 2" },
+        { id: 3, name: "Anuncio 3" },
+    ]);
+});
+app.listen(3333);
